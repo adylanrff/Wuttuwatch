807c872ea16fd765b2d66dd0d05743eea27a3027
adylanrff <adylanrff@gmail.com>
2017-08-08 20:48:50 +0700

    action bar icons and color

--- app/src/main/java/com/adylanroaffa/wuttuwatch/HomescreenActivity.java
+++ app/src/main/java/com/adylanroaffa/wuttuwatch/HomescreenActivity.java
@@ -1,17 +1,33 @@
 package com.adylanroaffa.wuttuwatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomescreenActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.homescreen_toolbar);
+        toolbar = (Toolbar) findViewById(R.id.homescreen_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.homescreen_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
