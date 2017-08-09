99cd030cd0adb8a565fae15e6d41f2299f0337c4
adylanrff <adylanrff@gmail.com>
2017-08-09 21:52:02 +0700

    homescreen fragment done

--- app/src/main/java/com/adylanroaffa/wuttuwatch/Fragments/TVShowsFragment.java
+++ app/src/main/java/com/adylanroaffa/wuttuwatch/Fragments/TVShowsFragment.java
@@ -24,6 +24,9 @@ public class TVShowsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_homescreen, container, false);
     }
 
